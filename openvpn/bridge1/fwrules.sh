1e383f3d76be14184f8dd856dda497f5b5951ff7
Spandan Bemby <spandan.777@gmail.com>
2016-07-30 16:35:21 +0000

    Removed bridge1

--- openvpn/bridge1/fwrules.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo iptables -A INPUT -i tap0 -j ACCEPT
-sudo iptables -A INPUT -i br0 -j ACCEPT
-sudo iptables -A FORWARD -i br0 -j ACCEPT
--- openvpn/bridge1/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook -i hosts --extra-vars="server_ip=54.205.138.151" openvpn.yaml
